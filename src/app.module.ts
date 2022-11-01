e9c5224c04f657309d5fb53851f4eeb05063a87e
xanedesu <xanedesu@gmail.com>
2022-11-01 10:16:59 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as Joi from 'joi';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        BROKER_URL: Joi.string().uri().required(),
+      }),
+    }),
+    AuthModule,
+  ],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
+})
+export class AppModule {}
